17f5c6b697019cceddef2cee443a08ddc7c3c530
sepinaco <sepinaco@gmail.com>
2022-03-23 17:35:12 +0100

    feat: added credits in homepage

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <>
     <h1>Scenes (temporal homepage)</h1>
+    <h2>Created by: joseangel aka sepinaco aka espinaco</h2>
     <Link href="/scenes/coronil">
-      <a>Coronil Scene</a>
+      <a>Coronil Scene (Music by: Alvaro Coronil)</a>
     </Link>
     </>
   )
