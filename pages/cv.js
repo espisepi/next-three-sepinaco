293e38717a60fa1dde6ccd35e59088ee472b3e7f
sepinaco <sepinaco@gmail.com>
2022-03-25 12:31:47 +0100

    change size cv

--- pages/cv.js
+++ pages/cv.js
@@ -4,7 +4,7 @@ import CVHtml from "../components/cv/CVHtml";
 
 export default function CV() {
     return (
-        <div className="CV-page" style={{width: '100vw', height: '100vh', display: 'flex'}}>
+        <div className="CV-page" style={{width: '80%', height: '100%', display: 'flex'}}>
             <CVHtml />
             {/* <CVCanvas /> */}
         </div>
