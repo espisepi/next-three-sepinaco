7507bcbf49cd71a00eb5f25e594bb32a6c0e76a4
sepinaco <sepinaco@gmail.com>
2022-03-25 12:27:06 +0100

    cv refactored

---
+++ components/cv/CVCanvas.js
@@ -0,0 +1,14 @@
+import { Canvas } from '@react-three/fiber';
+import { Box } from "@react-three/drei";
+
+
+
+export default function CVCanvas({style}) {
+    return (
+        <div className="CVCanvas-component" style={{  width: '40%', height: '100%', ...style}}>
+            <Canvas style={{backgroundColor: 'black'}}>
+                <Box />
+            </Canvas>
+        // </div>
+    )
+}
